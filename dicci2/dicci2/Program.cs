926aac4aa3ae9499c7ad52e6378031e76781ce1f
Axl Gómez <axljavigomez@gmail.com>
2026-03-07 21:17:57 -0600

    Creación del diccionario y añadir productos

---
+++ dicci2/dicci2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dicci2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> productos = new Dictionary<int,string>();
+            productos.Add(101,"Manzanas");
+            productos.Add(102, "Peras");
+            productos.Add(103, "Uvas");
+        }
+    }
+}
