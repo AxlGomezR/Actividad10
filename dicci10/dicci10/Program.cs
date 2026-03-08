28f7fe678cb6a326e4077cab4eaf07b4ca53723a
Axl Gómez <axljavigomez@gmail.com>
2026-03-08 13:24:18 -0600

    Creación de diccionario y añadiendo datos

--- dicci10/dicci10/Program.cs
+++ dicci10/dicci10/Program.cs
@@ -10,6 +10,11 @@ namespace dicci10
     {
         static void Main(string[] args)
         {
+            Dictionary<int,string> inventario = new Dictionary<int,string>();
+            inventario.Add(101, "Alambre");
+            inventario.Add(101, "Pegamento");
+            inventario.Add(101, "Martillo");
+            
         }
     }
 }
