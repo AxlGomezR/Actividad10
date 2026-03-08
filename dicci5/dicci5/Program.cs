26aa510aefffdfa10ac735f4375e200514dc7302
Axl Gómez <axljavigomez@gmail.com>
2026-03-07 21:44:49 -0600

    Imprimir la contraseña de un usuario por su clave

--- dicci5/dicci5/Program.cs
+++ dicci5/dicci5/Program.cs
@@ -14,7 +14,7 @@ namespace dicci5
             usuarios.Add("Javier1","Contra123");
             usuarios.Add("Sofi3", "Contraseña");
             usuarios.Add("Diego4", "123contra");
-
+            Console.WriteLine($"La contraseña del usuario Sofi3 es: {usuarios["Sofi3"]}");
         }
     }
 }
