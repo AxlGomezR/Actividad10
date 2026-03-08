0a5b5bc02f09b52705257816e51a1c17768be01a
Axl Gómez <axljavigomez@gmail.com>
2026-03-08 16:53:05 -0600

    Creación del menú y solicitar al usuario que seleccione una de las opciones

--- dicci19/dicci19/Program.cs
+++ dicci19/dicci19/Program.cs
@@ -10,6 +10,17 @@ namespace dicci19
     {
         static void Main(string[] args)
         {
+            Dictionary<string,double> notas = new Dictionary<string,double>();
+            int opcion;
+            do
+            {
+                Console.WriteLine("Seleccione una opción");
+                Console.WriteLine("1) Agregar nota");
+                Console.WriteLine("2) Modificar nota");
+                Console.WriteLine("3) Mostrar notas");
+                Console.WriteLine("4) Salir");
+                opcion = int.Parse(Console.ReadLine());
+            } while (opcion!=4);
         }
     }
 }
