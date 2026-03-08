17c838914d041d8852ff3d670056e7f336f8c1cb
Axl Gómez <axljavigomez@gmail.com>
2026-03-07 21:02:44 -0600

    commit4

--- dicci1/dicci1/Program.cs
+++ dicci1/dicci1/Program.cs
@@ -15,6 +15,11 @@ namespace dicci1
             Console.WriteLine("Prueba");
             Console.WriteLine("Prueba");
             Console.WriteLine("Prueba");
+            Console.WriteLine("Prueba");
+            Console.WriteLine("Prueba");
+            Console.WriteLine("Prueba");
+            Console.WriteLine("Prueba");
+            Console.WriteLine("Prueba");
         }
     }
 }
