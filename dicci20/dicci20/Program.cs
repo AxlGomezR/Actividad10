c37efc29ebdb57ec75dda5b6a99820bff4c2c7b9
Axl Gómez <axljavigomez@gmail.com>
2026-03-08 17:17:40 -0600

    Creación de ciclo que permite ingresar 3 veces un estudiante y solicitud del carné del estudiante

--- dicci20/dicci20/Program.cs
+++ dicci20/dicci20/Program.cs
@@ -10,6 +10,21 @@ namespace dicci20
     {
         static void Main(string[] args)
         {
+            Dictionary<int, (string, int, string)> estudiantes = new Dictionary<int, (string, int, string)>();
+            for(int i =1; i<=3; i++)
+            {
+                Console.WriteLine($"Ingrese el carné del estudiante {i}");
+                int carne = int.Parse(Console.ReadLine());
+                if (estudiantes.ContainsKey(carne))
+                {
+                    Console.WriteLine("El carné ingresado ya existe");
+                    i--;
+                }
+                else
+                {
+
+                }
+            }
         }
     }
 }
