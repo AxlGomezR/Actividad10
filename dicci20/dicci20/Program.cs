3e86b7cef0ca224b0768f3de13ff044355208e51
Axl Gómez <axljavigomez@gmail.com>
2026-03-08 17:21:48 -0600

    Solicitar nombre y edad. Verificar que se ingresó una opción válida

--- dicci20/dicci20/Program.cs
+++ dicci20/dicci20/Program.cs
@@ -11,18 +11,35 @@ namespace dicci20
         static void Main(string[] args)
         {
             Dictionary<int, (string, int, string)> estudiantes = new Dictionary<int, (string, int, string)>();
+            bool continuar;
             for(int i =1; i<=3; i++)
             {
                 Console.WriteLine($"Ingrese el carné del estudiante {i}");
                 int carne = int.Parse(Console.ReadLine());
                 if (estudiantes.ContainsKey(carne))
                 {
-                    Console.WriteLine("El carné ingresado ya existe");
+                    Console.WriteLine("El carné ingresado ya existe\n");
                     i--;
                 }
                 else
                 {
-
+                    Console.WriteLine("Ingrese el nombre del estudiante");
+                    string nombre = Console.ReadLine();
+                    continuar = false;
+                    do
+                    {
+                        Console.WriteLine("\nIngrese la edad del estudiante");
+                        int edad = int.Parse(Console.ReadLine());
+                        if (edad >= 0)
+                        {
+                            continuar = true;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Ingrese una opción válida\n");
+                        }
+                    } while (continuar==false);
                 }
             }
         }
